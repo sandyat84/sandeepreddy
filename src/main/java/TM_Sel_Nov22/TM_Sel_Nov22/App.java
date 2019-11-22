aa995c6b8e5bf29281843b9ebcb717bc8aae1e6d
admin <admin@SwathiSandy>
2019-11-22 09:35:32 +0530

    First Commit

---
+++ src/main/java/TM_Sel_Nov22/TM_Sel_Nov22/App.java
@@ -0,0 +1,13 @@
+package TM_Sel_Nov22.TM_Sel_Nov22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
